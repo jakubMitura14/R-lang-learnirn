e44e6ba1431934bba907db35ae43475311b700b1
jakubMitura14 <53857487+jakubMitura14@users.noreply.github.com>
2021-03-09 18:50:57 +0100

    up
    up

--- Coursera R course/R programming/week2/aa.R
+++ Coursera R course/R programming/week2/aa.R
@@ -8,21 +8,52 @@ pollutantmean <- function  ( pollutant,id ) {
   
 }
 
-getToTask1 <- function  ( frame, pollutant,ids ) { 
-  a <- longFrame %>% filter(
-    ID %in% ids )  
-  mean(a[[pollutant]], na.rm=TRUE)
+
+getnonNa <- function(name) {
+read_csv(paste("specdata/",name,".csv",sep="")) %>%
+  mutate(nanNumb = ifelse( !is.na(sulfate) &  !is.na(nitrate),1,0 )) %>%
+  .$nanNumb %>%
+  sum()}
+
+
+
+# when frame is given
+getnonNaFrame <- function(frame) {
+     frame  %>%
+    mutate(nanNumb = ifelse( !is.na(sulfate) &  !is.na(nitrate),1,0 )) %>%
+    .$nanNumb %>%
+    sum()}
+
+getCorrel <- function (frame){
+  frameA<-na.omit(frame)
+  cor(frameA$sulfate, frameA$nitrate )
 }
 
 
-#task 
-analyzeNa <- function() {
-myfiles = list.files(path=mydir, pattern="*.csv", full.names=TRUE)
-df <- data.frame(id=character(),nobs=double() ) 
-for(name in  myfiles) {
-table <- read_csv(paste("specdata/",name,".csv",sep=""))
-nob <- length(table[complete.cases(table),]$ID)
-df ->df %>% add_row(id = name, nobs =nob)
+fileNames <- myfiles %>% str_replace("\\.csv","")  %>% 
+  str_replace("specdata/","")
+
+frames <- map(fileNames,read_csv(paste("specdata/",.x,".csv",sep="")) )
+mappedNona <-  flatten_dbl(map(frames, getnonNaFrame))
+
+
+
+mapped<-map(fileNames, getnonNa)
+
+
+namees<- parse_number(fileNames)
+noNaa = flatten_dbl(mapped)
+tibble(names = namees, noNa = noNaa )
+
+main %>% filter(names %in%  c(6, 10, 20, 34, 100, 200, 310) )
+
+
+
+pThree <- function (main,treshold ){
+ numberNames-> main %>% filter(noNa > treshold ) # tribble with names column of number names that we are intrested in 
+  
 }
-df
-}
+
+
+vect <- filter(main, noNa>129)$corr
+
